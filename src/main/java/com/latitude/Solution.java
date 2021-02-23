47e81d05811ead1eeaa37c90965e85c21790a909
Temin <temin.email@gmail.com>
2021-02-23 20:13:32 +1100

    To add getMaxProfit() and unit test cases

--- src/main/java/com/latitude/Solution.java
+++ src/main/java/com/latitude/Solution.java
@@ -1,8 +1,49 @@
 package com.latitude;
 
+/**
+ * Solution Class
+ *
+ * @author Temin
+ *
+ */
 public class Solution {
+    /**
+     * A function for finding the maximum profit
+     *
+     * Assumption 1: stockPrice >= 0
+     * Assumption 2: When the best profit is less than zero, zero should be returned
+     *
+     * Preference: I prefer a long variable name for higher readability than comments
+     * because when code is changed, comment also needs to be changed.
+     * It means more maintenance cost
+     * This logic is not complicated so I don't think it needs a comment.
+     *
+     * @param stockPrices stock prices every minute from 10 am local time
+     * @return maximum benefit
+     */
     public int getMaxProfit(int[] stockPrices) {
+        if (stockPrices == null) {
+            return 0;
+        }
+        int stockPricesCount = stockPrices.length;
+        if (stockPricesCount < 2) {
+            return 0;
+        }
 
-        return 0;
+        int minStockPrice = stockPrices[0];
+        int maxBenefit = 0;
+        for (int stockPriceIndex = 1; stockPriceIndex < stockPricesCount; stockPriceIndex++) {
+            int currentStockPrice = stockPrices[stockPriceIndex];
+            int currentMaxBenefit = currentStockPrice - minStockPrice;
+
+            if (currentMaxBenefit > maxBenefit) {
+                maxBenefit = currentMaxBenefit;
+            }
+            if (minStockPrice > currentStockPrice) {
+                minStockPrice = currentStockPrice;
+            }
+        }
+
+        return maxBenefit;
     }
 }
--- src/test/java/com/latitude/SolutionTest.java
+++ src/test/java/com/latitude/SolutionTest.java
@@ -1,5 +1,100 @@
 package com.latitude;
 
-public class SolutionTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+/**
+ * Test for Solution Class
+ *
+ * @author Temin
+ *
+ */
+public class SolutionTest {
+    private void verifyGetMaxProfit(int[] prices, int profit) {
+        Solution solution = new Solution();
+        Assertions.assertEquals(profit, solution.getMaxProfit(prices));
+    }
+    
+    @Test
+    public void testNormalStockPrices() {
+        int[] stockPrices = {10, 7, 5, 8, 11, 9};
+        int expectedProfit = 6;
+        
+        verifyGetMaxProfit(stockPrices, expectedProfit);
+    }
+
+    @Test
+    public void testNullStockPrices() {
+        int expectedProfit = 0;
+
+        verifyGetMaxProfit(null, expectedProfit);
+    }
+
+    @Test
+    public void testEmptyStockPrices() {
+        int[] stockPrices = {};
+        int expectedProfit = 0;
+
+        verifyGetMaxProfit(stockPrices, expectedProfit);
+    }
+
+    @Test
+    public void testOneStockPrice() {
+        int[] stockPrices = {1};
+        int expectedProfit = 0;
+
+        verifyGetMaxProfit(stockPrices, expectedProfit);
+    }
+
+    @Test
+    public void testTwoStockPrices() {
+        int[] stockPrices = {1, 10};
+        int expectedProfit = 9;
+
+        verifyGetMaxProfit(stockPrices, expectedProfit);
+    }
+
+    @Test
+    public void testMaxStockPricesCount() {
+        // Max stock prices: 60min x 24hr
+        int[] stockPrices = new int[1440];
+        for (int stockPricesIndex = 0; stockPricesIndex < 1440; stockPricesIndex++) {
+            stockPrices[stockPricesIndex] = stockPricesIndex;
+        }
+        int expectedProfit = 1439;
+
+        verifyGetMaxProfit(stockPrices, expectedProfit);
+    }
+
+    @Test
+    public void testReducingStockPrices() {
+        int[] stockPrices = {9, 8, 7};
+        int expectedProfit = 0;
+
+        verifyGetMaxProfit(stockPrices, expectedProfit);
+    }
+
+    @Test
+    public void testSmallerAndSameStockPricesThanFirstPrice() {
+        int[] stockPrices = {9, 8, 8};
+        int expectedProfit = 0;
+
+        verifyGetMaxProfit(stockPrices, expectedProfit);
+    }
+
+    @Test
+    public void testSameStockPrices() {
+        int[] stockPrices = {1, 1, 1};
+        int expectedProfit = 0;
+
+        verifyGetMaxProfit(stockPrices, expectedProfit);
+    }
+
+    @Test
+    public void testMinAndMaxStockPrices() {
+        int[] stockPrices = {0, Integer.MAX_VALUE};
+        int expectedProfit = Integer.MAX_VALUE;
+
+        verifyGetMaxProfit(stockPrices, expectedProfit);
+    }
 }
